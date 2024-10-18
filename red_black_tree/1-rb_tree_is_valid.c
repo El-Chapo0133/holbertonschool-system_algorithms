06afa59367ade0ee1e4e8e6d0abcfd72d4ad0e69
El-Chapo0133 <levequeloris66@gmail.com>
2024-10-18 10:13:49 +0200

    Pushed last changes

--- red_black_tree/1-rb_tree_is_valid.c
+++ red_black_tree/1-rb_tree_is_valid.c
@@ -17,7 +17,7 @@ int rb_tree_is_valid(const rb_tree_t *tree)
 
 	if (!check_bst(tree))
 		return (false);
-	if (check_height(tree) != 0)
+	if (check_height(tree) > 1) /* should be 0 or 1 */
 		return (false);
 
 	return (true);
@@ -28,7 +28,7 @@ int rb_tree_is_valid(const rb_tree_t *tree)
  * the tree should be balanced and so not have an unbalanced side
  * @tree: parent node
  *
- * Return: value of the unbalancing
+ * Return: positive value of the unbalancing
  */
 int check_height(const rb_tree_t *tree)
 {
@@ -39,9 +39,18 @@ int check_height(const rb_tree_t *tree)
 		return (false);
 
 	difference = check_height(tree->left) - check_height(tree->right);
-	return (-difference + 1);
+	if (difference >= 0)
+		return (difference + 1);
+	return (-difference + 1); /* negative difference to get a positive */
 }
 
+/**
+ * check_bst - check if the tree is bst
+ * meaning smaller values on the left and bigger on the right
+ * @tree: parent node
+ *
+ * Return: bool
+ */
 int check_bst(const rb_tree_t *tree)
 {
 	if (!tree) /* NULL check */
@@ -50,6 +59,15 @@ int check_bst(const rb_tree_t *tree)
 	return (recursive_bst_check(tree, INT_MIN, INT_MAX));
 }
 
+/**
+ * recursive_bst_check - recursivly check is tree is bst
+ * meaning smaller values on the left and bigger on the right
+ * @tree: node
+ * @min: min acceptable value
+ * @max: max acceptable value
+ *
+ * Return: bool
+ */
 int recursive_bst_check(const rb_tree_t *tree, int min, int max)
 {
 	if (!tree) /* NULL check */
