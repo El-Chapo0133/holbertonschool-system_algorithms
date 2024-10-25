5b408e70293ae2b1d5b242df5305a2ddf1cc04c2
El-Chapo0133 <levequeloris66@gmail.com>
2024-10-25 11:19:20 +0200

    Pushed last changes

--- red_black_tree/3-array_to_rb_tree.c
+++ red_black_tree/3-array_to_rb_tree.c
@@ -19,7 +19,7 @@ rb_tree_t *array_to_rb_tree(int *array, size_t size)
 
 int is_duplicate(int *array, size_t size, int value)
 {
-	int index;
+	size_t index;
 	for (index = 0; index < size; index++)
 		if (array[index] == value)
 			return (true);
