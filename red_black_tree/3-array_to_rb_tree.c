d89c91f2c17e7827ac4bb3d0d56e2d5d76285112
El-Chapo0133 <levequeloris66@gmail.com>
2024-10-25 11:18:40 +0200

    Pushed last changes

--- red_black_tree/3-array_to_rb_tree.c
+++ red_black_tree/3-array_to_rb_tree.c
@@ -1,17 +1,27 @@
-#include "rb_trees.h"
+#include "3-array_to_rb_tree.h"
 
 
 rb_tree_t *array_to_rb_tree(int *array, size_t size)
 {
 	rb_tree_t *root = NULL;
-	size_t index_i;
+	size_t index;
 
 	if (array == NULL)
 		return (NULL);
 
-	for (index_i = 0; index_i < size; index_i++)
-		if (rb_tree_insert(&root, array[index_i]) == NULL)
-			return (NULL);
+	for (index = 0; index < size; index++)
+		if (rb_tree_insert(&root, array[index]) == NULL)
+			if (!is_duplicate(array, size, array[index]))
+				return (NULL);
 
 	return (root);
+}
+
+int is_duplicate(int *array, size_t size, int value)
+{
+	int index;
+	for (index = 0; index < size; index++)
+		if (array[index] == value)
+			return (true);
+	return (false);
 }
---
+++ red_black_tree/3-array_to_rb_tree.h
@@ -0,0 +1,10 @@
+#ifndef ARRAY_TO_RB_TREE
+#define ARRAY_TO_RB_TREE
+
+
+#include "rb_trees.h"
+
+int is_duplicate(int *array, size_t size, int value);
+
+
+#endif
