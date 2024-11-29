f4be401436b5f87031047e9377330722ddd7406e
El-Chapo0133 <levequeloris66@gmail.com>
2024-11-29 10:41:27 +0100

    Pushed last changes

--- graphs/2-graph_add_edge.c
+++ graphs/2-graph_add_edge.c
@@ -2,6 +2,30 @@
 
 
 /**
+ * find_vertex - find the vertex in the given graph
+ * from the given str
+ * @graph: graph
+ * @str: str to find
+ *
+ * Return: found vertex or NULL
+ */
+vertex_t *find_vertex(graph_t *graph, const char *str)
+{
+	vertex_t *temp;
+
+	if (!str)
+		return (NULL);
+	temp = graph->vertices;
+	while (temp)
+	{
+		if (strcmp(temp->content, str))
+			return (temp);
+		temp = temp->next;
+	}
+	return (NULL);
+}
+
+/**
  * edge_exists - get of the edge already exists
  * @v_src: vertex source
  * @v_dest: vertex_dest
