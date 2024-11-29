f0ef7670f24003c74ab43daf5a5d15dc31ef6f58
El-Chapo0133 <levequeloris66@gmail.com>
2024-11-29 09:37:22 +0100

    Pushed last changes

--- graphs/0-graph_create.c
+++ graphs/0-graph_create.c
@@ -1,9 +1,15 @@
 #include "graphs.h"
 
 
+/**
+ * graph_create - create a new graph_t
+ *
+ * Return: the newly created graph
+ */
 graph_t *graph_create(void)
 {
 	graph_t *new = malloc(sizeof(graph_t));
+
 	if (!new)
 		return (NULL);
 
