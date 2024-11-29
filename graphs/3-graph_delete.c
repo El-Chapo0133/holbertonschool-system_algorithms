e95e3afd5a7d290985f0e09332bcb2310f37ac91
El-Chapo0133 <levequeloris66@gmail.com>
2024-11-29 11:37:11 +0100

    Pushed last changes

--- graphs/3-graph_delete.c
+++ graphs/3-graph_delete.c
@@ -19,16 +19,19 @@ void graph_delete(graph_t *graph)
 	while (graph->vertices)
 	{
 		vertex = graph->vertices;
+		/* free all edges of this vertice */
 		while (vertex->edges)
 		{
 			edge = vertex->edges;
 			vertex->edges = edge->next;
 			free(edge);
 		}
+		/* free this vertice */
 		graph->vertices = vertex->next;
 		free(vertex->content);
 		free(vertex);
 	}
+	/* free this graph */
 	free(graph);
 	graph = NULL;
 }
