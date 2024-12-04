ba9eb7d3ed56793b91ba8b4e189c4b49034525e4
Loris Leveque <9601@holbertonstudents.com>
2024-12-04 14:23:57 +0100

    Pushed last changes

--- graphs/5-breadth_first_traverse.c
+++ graphs/5-breadth_first_traverse.c
@@ -105,8 +105,8 @@ size_t breadth_first_traverse(const graph_t *graph, void (*action)(const vertex_
 		return (0);
 
 	current = graph->vertices;
-	enqueue(queue, current->index);
 	enqueue(queue, LEVEL_BREAKER);
+	enqueue(queue, current->index);
 
 	while (queue->rear != -1)
 	{
