cc955e5c68624760ee0853b28c43810ca912a058
El-Chapo0133 <levequeloris66@gmail.com>
2024-12-20 14:02:49 +0100

    Pushed last changes

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -19,9 +19,14 @@ typedef struct symbol_s
     size_t freq;
 } symbol_t;
 
+/*  CORE  */
 symbol_t *symbol_create(char data, size_t freq);
 heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 int huffman_extract_and_insert(heap_t *priority_queue);
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size);
+
+/*  UTILS  */
+void free_data(void *data);
+int huffman_data_comparison(void *data1, void *data2);
 
 #endif
