91d49e69e67db2e478751c9d755c2587ff7d9b36
El-Chapo0133 <levequeloris66@gmail.com>
2024-12-20 14:03:40 +0100

    Pushed last changes

--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -40,6 +40,7 @@ binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
 			return (NULL);
 	
 	root = (binary_tree_node_t *)heap->root->data;
-	heap_delete(heap, free_data);
+/* 	heap_delete(heap, free_data);
+ */
 	return (root);
 }
