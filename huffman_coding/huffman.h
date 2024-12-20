945bc6b5956b60af24fe570e2ef5c60cb0326557
El-Chapo0133 <levequeloris66@gmail.com>
2024-12-20 10:07:13 +0100

    Pushed last changes

--- huffman_coding/huffman.h
+++ huffman_coding/huffman.h
@@ -16,4 +16,7 @@ typedef struct symbol_s
     size_t freq;
 } symbol_t;
 
+symbol_t *symbol_create(char data, size_t freq);
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
+
 #endif
