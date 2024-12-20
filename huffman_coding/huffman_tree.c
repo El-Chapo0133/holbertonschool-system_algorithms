fb2c48e113546fb58079a0246dd121442a4e28bb
El-Chapo0133 <levequeloris66@gmail.com>
2024-12-20 15:22:17 +0100

    Pushed last changes

--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -57,8 +57,9 @@ binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
 		if (!huffman_extract_and_insert(heap))
 			return (NULL);
 
-	root = (binary_tree_node_t *)heap->root;
-	free_whole_binary_tree(heap->root);
+	root = (binary_tree_node_t *)heap->root->data;
+/* 	free_whole_binary_tree(heap->root);
+ */
 	free(heap);
 	return (root);
 }
