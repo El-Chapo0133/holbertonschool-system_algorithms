14254fd2285dc26dc491f2e202b6f406b92253e5
El-Chapo0133 <levequeloris66@gmail.com>
2024-12-20 15:25:09 +0100

    Pushed last changes

--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -32,7 +32,7 @@ void free_whole_binary_tree(binary_tree_node_t *root)
 		free_whole_binary_tree(root->left);
 	if (root->right)
 		free_whole_binary_tree(root->right);
-	free_data(root);
+	free_data(root->data);
 }
 
 /**
